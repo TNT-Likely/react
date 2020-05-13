1bdc3d27d203cc0788c54124534b7f71ead8d7a0
likely <532837139@qq.com>
2020-05-13 19:59:21 +0800

    写着玩

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { PureComponent } from 'react'
+import ReactDOM from '../src/reactDom/index'
+import './index.less'
 
-console.log('Hello from tsx!')
+class App extends PureComponent {
+  state = {
+    list: []
+  }
+
+  render() {
+    const { list } = this.state
+    console.log("LIST", list)
+
+    return <div className="todo">
+      <input placeholder="添加代办事项" onKeyDown={this.keyDown} />
+      <h3>代办事项</h3>
+      {
+        list.length ? list.map((d, index) => d.isDone ? null : <p>
+          <input type="checkbox" />
+          {d.value}
+        </p>) : null
+      }
+      <h3>已办事项</h3>
+      {
+        list.length ? list.map((d, index) => !d.isDone ? null : <p>
+          <input type="checkbox" />
+          {d.value}
+        </p>) : null
+      }
+    </div>
+  }
+
+  componentDidMount() {
+    console.log("MOUNTED")
+  }
+
+  keyDown = e => {
+    const { list } = this.state
+
+    const value = e.target.value
+    if (e.keyCode === 13 && value) {
+      const newList = list.concat({
+        value,
+        isDone: false
+      })
+      this.setState({
+        list: newList
+      }, () => {
+        // e.target.value = ''
+      })
+    }
+  }
+}
 
 ReactDOM.render(
-  <p>Hello</p>,
+  <App />,
   document.getElementById('root'),
 )
--- src/reactDom/index.ts
+++ src/reactDom/index.ts
@@ -1,5 +1,79 @@
 const render = (vdom, container) => {
+    console.log("RENDER", vdom)
 
+    /** 判定自定义组件 */
+    if (vdom && typeof vdom.type === 'function') {
+        const cmp = new vdom.type()
+        console.log("CMP", vdom.type.prototype.render)
+        vdom = cmp.render()
+    }
+
+    console.log("AFTER", vdom)
+    
+    container.appendChild(createDOM(vdom))
+}
+
+const createDOM = vdom => {
+    if (typeof vdom === 'string' || !vdom) {
+        return document.createTextNode(vdom || '')
+    }
+
+    /** 属性列表 */
+    const attrs = Object.keys(vdom.props).filter(i => i !== 'children')
+
+    /** 创建dom */
+    const dom = document.createElement(vdom.type)
+
+    /** 设置属性值 */
+    attrs.forEach(attr => {
+        setAttribute(dom, attr, vdom.props[attr])
+    })
+    
+    const { children  = ''} = vdom.props
+    if (Array.isArray(children)) {
+        children.forEach(i => {
+            dom.appendChild(createDOM(i))
+        })
+    } else {
+        dom.appendChild(createDOM(children))
+    }
+    return dom
+}
+
+/**
+ * 设置属性
+ * @param dom 
+ * @param attr 属性
+ * @param value 属性值
+ */
+const setAttribute = (dom: HTMLElement, attr: string, value: any) => {
+    /** 重置classname */
+    if (attr === 'className') attr = 'class'
+
+    /** 样式设置 */
+    if (attr === 'style') {
+        if (typeof value === 'string') {
+            dom.style.cssText = value
+        }
+
+        if (typeof value === 'object') {
+            for(let i in value) {
+                dom.style[i] = value[i]
+            }
+        }
+    } else if (/^on\w+/.test(attr)) {
+        /** 匹配事件 */
+        attr = attr.toLowerCase()
+        dom[attr] = value
+    } else {
+        /** 通用属性设置 */
+        if (value) {
+            dom.setAttribute(attr, value)
+        } else {
+            dom.removeAttribute(attr)
+        }
+    }
+   
 }
 
 export default {
